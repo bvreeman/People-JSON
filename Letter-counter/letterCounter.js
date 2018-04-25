352753e8e63ef7463c8b78473d61afbfd81acf15
bvreeman <bvreeman@gmail.com>
2018-04-25 16:17:04 -0500

    got the lettercounter function working

--- Letter-counter/letterCounter.js
+++ Letter-counter/letterCounter.js
@@ -1,12 +1,26 @@
 // Write a function that takes a string and returns an object 
 // representing the character count for each letter.
 
-const letterCounter = string => {
+// const letterCounter = string => {
   // Awesome code here!! Use you mdn .reduce() docs!!
  let letters = 'abbcccddddeeeee'
-};
+ let name;
+ let lettersArray = Array.from(letters);
+//  console.log(lettersArray);
+var countedLetters = lettersArray.reduce(function (allLetters, letter) { 
+  if (letter in allLetters) {
+    allLetters[letter]++;
+  }
+  else {
+    allLetters[letter] = 1;
+  }
+  return allLetters;
+}, {});
+console.log(countedLetters);
 
-const result = letterCounter('abbcccddddeeeee')
-console.log(result)
+// };
+
+// const result = letterCounter('abbcccddddeeeee')
+// console.log(result)
 // Expected Output:
 // {a:1, b:2, c:3, d:4, e:5}
